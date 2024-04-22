6a09b4018a41985392b9fac8e03f7924fe3d5d60
Raphael Francisco Alves <canalcodigosimples@gmail.comm>
2024-04-21 21:53:04 -0300

    Adicionar arquivos de projeto.

---
+++ Core/Entities/TaskList.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities;
+
+public class TaskList
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int CountConcluded { get { return Tasks?.Count(x => x.IsConcluded) ?? 0; } }
+    public List<Tasks> Tasks { get; set; } = [];
+}
---
+++ Core/Entities/Tasks.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities;
+
+public class Tasks
+{
+    public int Id { get; set; }
+    public int ListId { get; set; }
+    public bool IsConcluded { get; set; } = false;
+    public string Title { get; set; }
+    public DateTime? DateConclusion { get; set; }
+    public bool IsImportant { get; set; } = false;
+}
